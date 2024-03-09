690049cb5805ec7a757093897c5569fd435cf086
glebokhlopkov <glebka_90@mail.ru>
2024-03-09 17:37:07 +0300

    create user authenticated/perms (create su) create habit perms correct models, migrations

--- habit/models.py
+++ habit/models.py
@@ -11,14 +11,14 @@ class Habit(models.Model):
         ('DAY', 'one time in day'),
         ('WEEK', 'one time in week'),
     ]
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='owner')
+    owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, verbose_name='owner')
     place = models.CharField(max_length=200, verbose_name='place')
     time = models.TimeField(verbose_name='time')
     action = models.CharField(max_length=200, verbose_name='action')
     is_nice_habit = models.BooleanField(null=True, blank=True, verbose_name='is_nice_habit')
     nice_habit = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='nice_habit')
     period = models.CharField(max_length=20, choices=PERIOD_OF_HABIT, default='DAY', verbose_name='period')
-    reward = models.CharField(max_length=200, verbose_name='reward')
+    reward = models.CharField(max_length=200, null=True, blank=True, verbose_name='reward')
     duration = models.DurationField(default=timedelta(minutes=2), verbose_name='duration')
     is_public = models.BooleanField(default=False, verbose_name='is_public')
 
