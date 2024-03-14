702fc300a2adca8881ea77221e6aa2f5459f20fd
glebokhlopkov <glebka_90@mail.ru>
2024-03-14 18:06:58 +0300

    correct validators.py

--- habit/validators.py
+++ habit/validators.py
@@ -2,31 +2,66 @@ from datetime import timedelta
 
 from rest_framework import serializers
 
+from habit.models import Habit
 
-def validator_habit_fields(value):
-    try:
-        if value['is_nice_habit']:
-            if value['nice_habit'] or value['reward']:
-                raise serializers.ValidationError(
-                    'Nice habit cant have reward or another nice habit')
-    except KeyError:
-        pass
-    try:
-        if value['nice_habit'] and value['reward']:
+
+class RewardOrNiceHabitValidator:
+
+    def __call__(self, value):
+        nice_habit = bool(dict(value).get('nice_habit'))
+        reward = bool(dict(value).get('reward'))
+
+        if nice_habit and reward:
             raise serializers.ValidationError(
                 'Habit can have only reward or nice habit')
-    except KeyError:
-        pass
-    try:
-        if value['nice_habit']:
-            if not value['nice_habit'].is_nice_habit:
+
+
+class IsNiceHabitValidator:
+
+    def __call__(self, value):
+        nice_habit = dict(value).get('nice_habit')
+        if nice_habit:
+            habit = Habit.objects.get(pk=nice_habit.id)
+            if not habit.is_nice_habit:
                 raise serializers.ValidationError(
                     'Habit can connected only with nice habit')
-    except KeyError:
-        pass
-    try:
-        if value['duration'] > timedelta(minutes=2):
+
+
+class IsNiceHabitHaveRewardValidator:
+
+    def __call__(self, value):
+        is_nice_habit = dict(value).get('is_nice_habit')
+        reward = bool(dict(value).get('reward'))
+        nice_habit = bool(dict(value).get('nice_habit'))
+
+        if is_nice_habit and reward or is_nice_habit and nice_habit:
             raise serializers.ValidationError(
-                'Habits action duration must be less than 2 min (120 sec)')
-    except KeyError:
-        pass
+                'Nice habit cant have reward or another nice habit')
+
+# def validator_habit_fields(value):
+#     try:
+#         if value['is_nice_habit']:
+#             if value['nice_habit'] or value['reward']:
+#                 raise serializers.ValidationError(
+#                     'Nice habit cant have reward or another nice habit')
+#     except KeyError:
+#         pass
+#     try:
+#         if value['nice_habit'] and value['reward']:
+#             raise serializers.ValidationError(
+#                 'Habit can have only reward or nice habit')
+#     except KeyError:
+#         pass
+#     try:
+#         if value['nice_habit']:
+#             if not value['nice_habit'].is_nice_habit:
+#                 raise serializers.ValidationError(
+#                     'Habit can connected only with nice habit')
+#     except KeyError:
+#         pass
+#     try:
+#         if value['duration'] > timedelta(minutes=2):
+#             raise serializers.ValidationError(
+#                 'Habits action duration must be less than 2 min (120 sec)')
+#     except KeyError:
+#         pass
